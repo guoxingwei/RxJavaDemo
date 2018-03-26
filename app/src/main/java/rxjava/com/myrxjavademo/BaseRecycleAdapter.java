350bc31deb43d7ab2ca187342e3d6543b0b9ac89
guoxingwei <GuoToGo_518@Yeah.net>
2018-03-26 22:33:29 +0800

    Initial commit

---
+++ app/src/main/java/rxjava/com/myrxjavademo/BaseRecycleAdapter.java
@@ -0,0 +1,8 @@
+package rxjava.com.myrxjavademo;
+
+/**
+ * Created by Administrator on 2018/2/25.
+ */
+
+public class BaseRecycleAdapter {
+}
---
+++ app/src/main/java/rxjava/com/myrxjavademo/RxJavaManager.java
@@ -0,0 +1,8 @@
+package rxjava.com.myrxjavademo;
+
+/**
+ * Created by Administrator on 2018/3/15.
+ */
+
+public class RxJavaManager {
+}
